32b8c97018a1c2fd4d0cc501568ab1f992a9e2fe
Cameron Schneider <cameron.schneider@mymail.champlain.edu>
2021-03-08 14:29:36 -0600

    Adding in basic windowing w/ SDL and the vulkan library!

---
+++ vulkan-guide-starting-point/src/vk_engine.cpp
@@ -0,0 +1,61 @@
+﻿
+#include "vk_engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_types.h>
+#include <vk_initializers.h>
+
+void VulkanEngine::init()
+{
+	// We initialize SDL and create a window with it. 
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_VULKAN);
+	
+	window = SDL_CreateWindow(
+		"Vulkan Engine",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		windowExtent.width,
+		windowExtent.height,
+		window_flags
+	);
+	
+	//everything went fine
+	isInitialized = true;
+}
+void VulkanEngine::cleanup()
+{	
+	if (isInitialized) {
+		
+		SDL_DestroyWindow(window);
+	}
+}
+
+void VulkanEngine::draw()
+{
+	//nothing yet
+}
+
+void VulkanEngine::run()
+{
+	SDL_Event e;
+	bool bQuit = false;
+
+	//main loop
+	while (!bQuit)
+	{
+		//Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//close the window when user alt-f4s or clicks the X button			
+			if (e.type == SDL_QUIT)
+				bQuit = true;
+		}
+
+		draw();
+	}
+}
+
---
+++ vulkan-guide-starting-point/src/vk_engine.h
@@ -0,0 +1,29 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vk_types.h>
+
+class VulkanEngine {
+public:
+
+	bool isInitialized{ false };
+	int frameNumber {0};
+
+	VkExtent2D windowExtent{ 1700 , 900 };
+
+	struct SDL_Window* window{ nullptr };
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+};
